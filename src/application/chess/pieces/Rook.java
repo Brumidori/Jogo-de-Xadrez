30e257bd0d897c262a2b19b75bff5248c1600c84
bru_midori <brunamidoriyy@yahoo.com.br>
2023-02-01 10:23:28 -0300

    Implement possible moves of Rock

--- src/application/chess/pieces/Rook.java
+++ src/application/chess/pieces/Rook.java
@@ -3,6 +3,7 @@ package application.chess.pieces;
 import application.chess.ChessPiece;
 import application.chess.Color;
 import boardgame.Board;
+import boardgame.Position;
 
 //torre
 public class Rook extends ChessPiece {
@@ -19,6 +20,47 @@ public class Rook extends ChessPiece {
     @Override
     public boolean[][] possibleMovies() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+
+        //above
+        p.setValues(position.getRow() -1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] =true;
+            p.setRow(p.getRow()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //left
+        p.setValues(position.getRow(), position.getColumn()-1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] =true;
+            p.setColumn(p.getColumn()-1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+
+        //right
+        p.setValues(position.getRow(), position.getColumn()+1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] =true;
+            p.setColumn(p.getColumn()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+
+        //below
+        p.setValues(position.getRow() +1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+            mat[p.getRow()][p.getColumn()] =true;
+            p.setRow(p.getRow()+1);
+        }
+        if(getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+
         return mat;
     }
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -32,4 +32,9 @@ public class Position {
     public void setColumn(int column) {
         this.column = column;
     }
+
+    public void setValues(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
 }

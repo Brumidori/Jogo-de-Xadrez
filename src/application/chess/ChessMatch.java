c186a3c8852fdc992e0bca7e4fbeac0131c4e45e
bru_midori <brunamidoriyy@yahoo.com.br>
2023-02-09 17:04:30 -0300

    specialmove promotion refactoring

--- src/application/chess/ChessMatch.java
+++ src/application/chess/ChessMatch.java
@@ -2,11 +2,9 @@ package application.chess;
 
 import application.chess.pieces.*;
 import boardgame.Board;
-import boardgame.BoardException;
 import boardgame.Piece;
 import boardgame.Position;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -111,8 +109,8 @@ public class ChessMatch {
         if(promoted == null){
             throw new IllegalStateException("There is no piece to be promoted");
         }
-        if(!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+        if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
